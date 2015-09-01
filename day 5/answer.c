15afd9aac27f2ceb51f2dec4be16a310bf0e072d
Arka Prava Basu <arkaprava94@gmail.com>
2015-09-01 15:54:06 +0530

    Day 5 assignments

---
+++ day 5/answer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k;
+	printf();
+	for(i=0;i<n;i++)
+	{
+		 c++;
+		
+	}
+	return 0;
+	
+}
---
+++ day 5/indent.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int i,j,k;printf();
+for(i=0;i<n;i++)
+{ c++;
+}
+return 0;
+}
+
