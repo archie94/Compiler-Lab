8890228f210ecf772365825943d9fbd1601249a3
Arka Prava Basu <arkaprava94@gmail.com>
2015-10-12 07:47:44 +0530

    basic.c added

---
+++ day 3/basic.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int i,j,k;//initialiasation
+	char c;//another one //
+	//hello
+	/*do some 
+	work 
+	after this line*/
+	scanf("%d",&i);
+	printf("%d",i);
+	/*lets see
+	if this 
+	
+	works*/
+/* comment inside// comment is ignored */
+	/* this is a comment
+	// this is ignored
+	*/
+	return 0;
+}
+
