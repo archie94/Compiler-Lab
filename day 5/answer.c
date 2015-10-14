1172262e9d99890666dd3185b8005df2f53d1c2b
Arka Prava Basu <arkaprava94@gmail.com>
2015-10-14 09:47:06 +0530

    Revised day 5 assignment

--- day 5/answer.c
+++ day 5/answer.c
@@ -5,7 +5,7 @@ int main()
 	printf();
 	for(i=0;i<n;i++)
 	{
-		 c++;
+		c++;
 		
 	}
 	return 0;
--- day 5/indent.c
+++ day 5/indent.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int i,j,k;printf();
+int i,j,k;        printf();
 for(i=0;i<n;i++)
 { c++;
 }
